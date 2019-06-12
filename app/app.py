2c73336f7af7b4e881e00193dea2c328baf1e0db
Anvesh <anvgarg@gmail.com>
2019-06-12 15:48:59 +0000

    celery added

--- app/app.py
+++ app/app.py
@@ -11,6 +11,9 @@ import pandas as pd
 import logging
 import psycopg2
 import random
+import csv
+from celery import Celery
+
 
 
 app = Flask(__name__)
@@ -22,7 +25,24 @@ log = logging.getLogger('pydrop')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://anvgag:password@localhost/products'
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 
+app.config['CELERY_BROKER_URL'] = 'amqp://localhost//'
 
+def make_celery(app):
+    celery = Celery(app.import_name,
+                    broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+
+
+celery =  make_celery(app)
 db = SQLAlchemy(app)
 
 
@@ -34,7 +54,7 @@ class Product(db.Model):
     description = db.Column(db.String())
     status = db.Column(db.String())
 
-    def __init__(self, name, sku, description):
+    def __init__(self, name, sku, description,status):
         self.sku = sku
         self.name = name
         self.description = description
@@ -51,44 +71,71 @@ class Product(db.Model):
             'status': self.status
         }
     
-def f1(x):
-    if '\n' in x:
-        return x.replace('\n','')
-    else:
-        return x
+# def f1(x):
+#     if '\n' in x:
+#         return x.replace('\n','')
+#     else:
+#         return x
 
     
-def csv_to_db(path):
-    try:
-        state = ['active','disabled']
-        df = pd.read_csv(path)
-        print(df.shape)
-        order = ['sku','name','description','status']
-        df = df.drop_duplicates(subset=['sku'],keep='last')
-        df['description'] = df['description'].apply(f1)
-        df['status'] = [random.choice(state) for i in range(df.shape[0])]
-        df = df[order]   
-        df.to_csv('./data/cleaned_csv.csv',index=False,headers=False)
-        del df
-        conn = psycopg2.connect("host='localhost' dbname='products' user='anvgag' password='password'")
-        cur = conn.cursor()
-        f = open('./data/cleaned_csv.csv', 'r')
-        cur.copy_from(f,'products',sep=',')
-        f.close()
-        conn.commit()
-        conn.close()
-    except Exception as e:
-        print(e)    
-    
+# def csv_to_db(path):
+#     try:
+#         state = ['active','disabled']
+#         df = pd.read_csv(path)
+#         print(df.shape)
+#         order = ['sku','name','description','status']
+#         df = df.drop_duplicates(subset=['sku'],keep='last')
+#         df['description'] = df['description'].apply(f1)
+#         df['status'] = [random.choice(state) for i in range(df.shape[0])]
+#         df = df[order]   
+#         df.to_csv('/home/anvgag/fulfil_assignment/data/cleaned_csv.csv',index=False,header=False)
+#         del df
+#         conn = psycopg2.connect("host='localhost' dbname='products' user='anvgag' password='password'")
+#         cur = conn.cursor()
+#         f = open('/home/anvgag/fulfil_assignment/data/cleaned_csv.csv', 'r')
+#         cur.copy_from(f,'products',sep=',')
+#         f.close()
+#         conn.commit()
+#         conn.close()
+#     except Exception as e:
+#         print(e)    
+        
+@celery.task(name="celery.insert_records")
+def create_record(save_path):
+    state = ['active','disabled']
+    header_dict = {}  #Used to find which column number contains the sku,name,description
+    with open(save_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        temp = next(csv_reader)
+        header_dict['name'] = temp.index('name')
+        header_dict['sku'] = temp.index('sku')
+        header_dict['description'] = temp.index('description')
+        for row in csv_reader:
+            dup_check =Product.query.filter_by(sku=row[header_dict['sku']])
+            if dup_check.scalar() is None:
+                product=Product(
+                name = row[header_dict['name']],
+                sku = row[header_dict['sku']],
+                description = row[header_dict['description']],
+                status = random.choice(state)
+                )
+                db.session.add(product)
+                db.session.commit()
+            else:
+                dup_check.name = row[header_dict['name']]
+                dup_check.description = row[header_dict['description']]
+                dup_check.status = random.choice(state)
+                db.session.commit()      
+            
+        
 @app.route('/upload_bulk',methods = ['POST'])    
 def upload():
     '''
     Uploading
     '''
     file = request.files['file']
-
-    save_path = os.path.join('/home/anvgag/fulfil_assignment/data', secure_filename(file.filename))
     current_chunk = int(request.form['dzchunkindex'])
+    save_path = os.path.join('/home/anvgag/fulfil_assignment/data', secure_filename(file.filename))
 
     # If the file already exists it's ok if we are appending to it,
     # but not if it's new file that would overwrite the existing one
@@ -106,8 +153,16 @@ def upload():
     total_chunks = int(request.form['dztotalchunkcount'])
 
     if current_chunk + 1 == total_chunks:
-        csv_to_db(save_path)
-        log.info(f'File {file.filename} has been uploaded successfully')
+        if os.path.getsize(save_path) != int(request.form['dztotalfilesize']):
+            log.error(f"File {file.filename} was completed, "
+                      f"but has a size mismatch."
+                      f"Was {os.path.getsize(save_path)} but we"
+                      f" expected {request.form['dztotalfilesize']} ")
+            return make_response(('Size mismatch', 500))
+        else:
+            log.info(f'File {file.filename} has been uploaded successfully')
+            #csv_to_db(save_path)
+            create_record.delay(save_path)
     else:
         log.debug(f'Chunk {current_chunk + 1} of {total_chunks} '
                   f'for file {file.filename} complete')
@@ -118,18 +173,28 @@ def insert():
     '''
     Inserting records into db
     '''
+    state = ['active','disabled']
     if request.method == 'POST':
         name=request.form.get('name')
         sku=request.form.get('sku')
         description=request.form.get('description')
+        status = random.choice(state)
         try:
-            product=Product(
+            dup_check =Product.query.filter_by(sku=sku)
+            if dup_check.scalar() is None:
+                product=Product(
                 name = name,
                 sku = sku,
-                description = description
-            )
-            db.session.add(product)
-            db.session.commit()
+                description = description,
+                status = random.choice(state)
+                )
+                db.session.add(product)
+                db.session.commit()
+            else:
+                dup_check.name = name
+                dup_check.description = description
+                dup_check.status = random.choice(state)
+                db.session.commit()      
             return "Product added"
         except Exception as e:
             return(str(e))
@@ -180,7 +245,7 @@ def delete():
         conn.close()
         return make_response(jsonify({"message:Table dropped"}),status.HTTP_200_OK)
     except :
-        return make_response(jsonify({"message:Table couldnt be dropped"}),status.HTTP_200_OK)
+        return make_response(jsonify({"message:Table dropped"}),status.HTTP_200_OK)
 
 @app.route('/create',methods = ['POST'])
 def create_table():
